663422f0b2b587cee54b2dfbe90616ab835ba2b1
Salman Shah <sbs.191197@gmail.com>
2018-03-29 00:09:46 +0530

    Documentation and Comments to Python code added

--- Sieve/Sieve.py
+++ Sieve/Sieve.py
@@ -1,8 +1,14 @@
+'''
+Sieve of Eratosthenes
+Input: Integer n
+Output: Intger 
+'''
+
 def sieve(n):
     not_prime = []
     for i in xrange(2, n+1):
         if i not in not_prime:
-            print i
+            print(i)
             for j in xrange(i*i, n+1, i):
                 not_prime.append(j)
 
